87eae0d8913c1628fceb2cc0a25d86b1beec1a72
yuzumone <yuzumone@outlook.com>
2023-05-17 19:11:54 +0900

    Update auto_route

--- lib/ui/route/app_router.gr.dart
+++ lib/ui/route/app_router.gr.dart
@@ -1,64 +1,91 @@
-// **************************************************************************
-// AutoRouteGenerator
-// **************************************************************************
-
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
 // **************************************************************************
-// AutoRouteGenerator
+// AutoRouterGenerator
 // **************************************************************************
 
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:auto_route/auto_route.dart' as _i3;
-import 'package:flutter/material.dart' as _i4;
-import 'package:grandis/data/model/park_type.dart' as _i5;
+import 'package:flutter/material.dart' as _i5;
+import 'package:grandis/data/model/park_type.dart' as _i4;
 import 'package:grandis/ui/detail/detail_page.dart' as _i2;
 import 'package:grandis/ui/home/home_page.dart' as _i1;
 
-class AppRouter extends _i3.RootStackRouter {
-  AppRouter([_i4.GlobalKey<_i4.NavigatorState>? navigatorKey])
-      : super(navigatorKey);
+abstract class $AppRouter extends _i3.RootStackRouter {
+  $AppRouter({super.navigatorKey});
 
   @override
   final Map<String, _i3.PageFactory> pagesMap = {
     HomeRoute.name: (routeData) {
-      return _i3.AdaptivePage<dynamic>(
-          routeData: routeData, child: const _i1.HomePage());
+      return _i3.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const _i1.HomePage(),
+      );
     },
     DetailRoute.name: (routeData) {
       final args = routeData.argsAs<DetailRouteArgs>();
-      return _i3.AdaptivePage<dynamic>(
-          routeData: routeData,
-          child: _i2.DetailPage(type: args.type, key: args.key));
-    }
+      return _i3.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: _i2.DetailPage(
+          type: args.type,
+          key: args.key,
+        ),
+      );
+    },
   };
-
-  @override
-  List<_i3.RouteConfig> get routes => [
-        _i3.RouteConfig(HomeRoute.name, path: '/'),
-        _i3.RouteConfig(DetailRoute.name, path: '/detail-page')
-      ];
 }
 
-/// generated route for [_i1.HomePage]
+/// generated route for
+/// [_i1.HomePage]
 class HomeRoute extends _i3.PageRouteInfo<void> {
-  const HomeRoute() : super(name, path: '/');
+  const HomeRoute({List<_i3.PageRouteInfo>? children})
+      : super(
+          HomeRoute.name,
+          initialChildren: children,
+        );
 
   static const String name = 'HomeRoute';
+
+  static const _i3.PageInfo<void> page = _i3.PageInfo<void>(name);
 }
 
-/// generated route for [_i2.DetailPage]
+/// generated route for
+/// [_i2.DetailPage]
 class DetailRoute extends _i3.PageRouteInfo<DetailRouteArgs> {
-  DetailRoute({required _i5.ParkType type, _i4.Key? key})
-      : super(name,
-            path: '/detail-page', args: DetailRouteArgs(type: type, key: key));
+  DetailRoute({
+    required _i4.ParkType type,
+    _i5.Key? key,
+    List<_i3.PageRouteInfo>? children,
+  }) : super(
+          DetailRoute.name,
+          args: DetailRouteArgs(
+            type: type,
+            key: key,
+          ),
+          initialChildren: children,
+        );
 
   static const String name = 'DetailRoute';
+
+  static const _i3.PageInfo<DetailRouteArgs> page =
+      _i3.PageInfo<DetailRouteArgs>(name);
 }
 
 class DetailRouteArgs {
-  const DetailRouteArgs({required this.type, this.key});
+  const DetailRouteArgs({
+    required this.type,
+    this.key,
+  });
 
-  final _i5.ParkType type;
+  final _i4.ParkType type;
 
-  final _i4.Key? key;
+  final _i5.Key? key;
+
+  @override
+  String toString() {
+    return 'DetailRouteArgs{type: $type, key: $key}';
+  }
 }
